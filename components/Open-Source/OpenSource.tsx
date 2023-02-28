6b09f07e44fdde9ed34e0be1f9be4098a9f2e299
Mohit-Aasirwal <mohitaasirwal@gmail.com>
2023-03-01 02:00:45 +0530

    opensource UI and structuring

--- components/Open-Source/OpenSource.tsx
+++ components/Open-Source/OpenSource.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Astronomer from "../../assets/SVG/astronomer";
+import Star from "../../assets/SVG/star";
 
 const OpenSource = () => {
   return (
-    <div>OpenSource</div>
-  )
-}
+    <div className="flex flex-row bg-secondaryblack">
+      <div className="w-1/3 h-screen specialdiv4 bg-darkblack"></div>
+      <div className="w-2/3 h-screen flex">
+        <Astronomer height={100} width={100} className={`my-10`} />
+        <h1 className="uppercase font-['Encode Sans SC'] text-5xl px-56 w-full">
+          Open-Source
+        </h1>
+      </div>
+      <Star width={50} height={50} className={`relative top-[40rem]`}/>
+    </div>
+  );
+};
 
-export default OpenSource
+export default OpenSource;
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -4,11 +4,11 @@ import Telescope from "../../assets/SVG/telescope";
 const Projects = () => {
   return (
     <div className="flex h-screen flex-row bg-secondaryblack">
-      <div className=" w-2/3 my-20 flex flex-col">
+      <div className=" w-2/3 my-20 flex">
         <h1 className="uppercase font-['Encode Sans SC'] text-5xl my-20 w-full text-center">
           Projects
         </h1>
-        <Telescope width={100} height={100} className={`opacity-20`} />
+        <Telescope width={100} height={100} className={`opacity-20 my-20`} />
       </div>
       <div className="specialdiv2 h-screen w-1/3 bg-darkblack"></div>
     </div>
